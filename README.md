d4bb736314208f60c2f88f71ae7dc3bc66b7e779
Sebastian Aranguiz <sarang575@gmail.com>
2022-12-02 03:33:36 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Serverless HTTP Proxy
+
+> Krypto Ledgers SpA
+
+A Deno AWS Lambda serverless HTTP proxy
+
+## Reference
+
+[Deno on AWS Lambda](https://github.com/hayd/deno-lambda)
+
+[CORS proxy](https://github.com/Glifery/cors-proxy)
---
+++ handler.ts
@@ -0,0 +1,55 @@
+import {
+  APIGatewayProxyEventV2,
+  APIGatewayProxyResultV2,
+} from "https://deno.land/x/lambda@1.28.2/mod.ts";
+
+export async function handler(
+  event: APIGatewayProxyEventV2,
+): Promise<APIGatewayProxyResultV2> {
+  const proxy = event.pathParameters?.proxy;
+  const method = event.requestContext.http.method;
+  const headers = event.headers;
+  const params = event.queryStringParameters;
+  const body = event.body;
+
+  if (!proxy) {
+    return {
+      statusCode: 400,
+      body: "Missing `url` path parameter",
+    };
+  }
+
+  const url = new URL(proxy);
+
+  if (params) {
+    for (const [name, value] of Object.entries(params)) {
+      if (value) url.searchParams.set(name, value);
+    }
+  }
+
+  const requestHeaders = new Headers();
+
+  for (const [name, value] of Object.entries(headers)) {
+    if (/(host|origin)/i.test(name)) continue;
+    if (value) requestHeaders.set(name, value);
+  }
+
+  const requestBody = /(POST|PUT)/i.test(method) ? body : null;
+
+  const response = await fetch(url, {
+    method,
+    body: requestBody,
+    headers: requestHeaders,
+  });
+
+  const responseContentType = response.headers.get("content-type");
+  const responseBody = await response.text();
+
+  return {
+    statusCode: response.status,
+    body: responseBody,
+    headers: responseContentType
+      ? { "content-type": responseContentType }
+      : undefined,
+  };
+}
